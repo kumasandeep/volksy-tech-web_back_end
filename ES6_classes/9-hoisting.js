5734e22298c04801d69e697de8badbe3522ed34e
root <you@example.com>
2023-03-06 11:48:21 +0000

    l

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -12,10 +12,8 @@ class HolbertonClass {
     return this._location;
   }
 }
-
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -31,12 +29,10 @@ class StudentHolberton {
     return this.holbertonClass;
   }
 }
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
 const listOfStudents = [student1, student2, student3, student4, student5];
 export default listOfStudents;
